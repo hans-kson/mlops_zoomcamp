bf840530604ba9de2e02122bdead9d2a365a6676
hans-kson <76441783+hans-kson@users.noreply.github.com>
2022-06-10 22:54:32 +0200

    done with q5 homework

---
+++ 03-orchestration/homework_hkn.py
@@ -0,0 +1,148 @@
+import pandas as pd
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+#import mlflow
+from prefect import flow, task, get_run_logger
+from prefect.task_runners import SequentialTaskRunner
+
+import re
+import pickle
+
+@task
+def read_data(path):
+    df = pd.read_parquet(path)
+    return df
+
+@task
+def prepare_features(df, categorical, train=True):
+    logger = get_run_logger()
+    df['duration'] = df.dropOff_datetime - df.pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    mean_duration = df.duration.mean()
+    if train:
+        print(f"The mean duration of training is {mean_duration}")
+        logger.info(f"The mean duration of training is {mean_duration}")
+        
+    else:
+        print(f"The mean duration of validation is {mean_duration}")
+        logger.info(f"The mean duration of validation is {mean_duration}")
+    
+    
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    return df
+
+@task
+def train_model(df, categorical):
+    logger = get_run_logger()
+    train_dicts = df[categorical].to_dict(orient='records')
+    dv = DictVectorizer()
+    X_train = dv.fit_transform(train_dicts) 
+    y_train = df.duration.values
+
+    print(f"The shape of X_train is {X_train.shape}")
+    print(f"The DictVectorizer has {len(dv.feature_names_)} features")
+    logger.info(f"The shape of X_train is {X_train.shape}")
+    logger.info(f"The DictVectorizer has {len(dv.feature_names_)} features")
+
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    y_pred = lr.predict(X_train)
+    mse = mean_squared_error(y_train, y_pred, squared=False)
+    print(f"The MSE of training is: {mse}")
+    logger.info(f"The MSE of training is: {mse}")
+    
+    return lr, dv
+
+@task
+def run_model(df, categorical, dv, lr):
+    logger = get_run_logger()
+    val_dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(val_dicts) 
+    y_pred = lr.predict(X_val)
+    y_val = df.duration.values
+
+    mse = mean_squared_error(y_val, y_pred, squared=False)
+    print(f"The MSE of validation is: {mse}")
+    logger.info(f"The MSE of validation is: {mse}")
+    
+    return
+
+@task
+def get_paths(date):
+    string = str(date)
+    chop = string.split('-')
+    year=chop[0]
+
+    train_month = int(chop[1])-2
+    if train_month >=1:
+        train_year=year
+        train_month = '0'+str(train_month)
+    else:
+        train_year = str(int(year)-1)
+        train_month=str(12+train_month)
+
+    val_month = int(chop[1])-1
+    if val_month >=1:
+        val_year=year
+        val_month = '0'+str(val_month)
+    else:
+        val_year = str(int(year)-1)
+        val_month=str(12+val_month)
+
+    train_path = './data/fhv_tripdata_'+train_year+'-'+train_month+'.parquet'
+    val_path = './data/fhv_tripdata_'+val_year+'-'+val_month+'.parquet'
+
+    return train_path, val_path    
+
+@flow(task_runner=SequentialTaskRunner())
+def main(date=None):
+    #train_path: str = './data/fhv_tripdata_2021-01.parquet', 
+    #val_path: str = './data/fhv_tripdata_2021-02.parquet'
+
+    train_path, val_path = get_paths(date).result()
+    
+    categorical = ['PUlocationID', 'DOlocationID']
+
+    df_train = read_data(train_path)
+    df_train_processed = prepare_features(df_train, categorical)
+
+    df_val = read_data(val_path)
+    df_val_processed = prepare_features(df_val, categorical, False)
+
+    
+    # train the model
+    lr, dv = train_model(df_train_processed, categorical).result()
+    run_model(df_val_processed, categorical, dv, lr)
+
+    #save model and dv
+    dv_path = './models/dv-'+date+'.b'
+    with open(dv_path, "wb") as f_out:
+        pickle.dump(dv, f_out)
+    
+    model_path = './models/model-'+date+'.bin'
+    with open(model_path, "wb") as f_out:
+        pickle.dump(lr, f_out)
+
+    from prefect.deployments import DeploymentSpec
+    from prefect.orion.schemas.schedules import CronSchedule
+    from prefect.flow_runners import SubprocessFlowRunner
+
+    DeploymentSpec(
+        name="cron-schedule-deployment",
+        #flow_location="./homework_hkn.py",
+        flow=main,
+        flow_runner=SubprocessFlowRunner(),
+        tags=['ml'],
+        #parameters={'date':"2021-08-15"},
+        schedule=CronSchedule(
+            cron="0 9 15 * *",
+           timezone="America/New_York"),
+           )
+
+
+main(date="2021-08-15")
